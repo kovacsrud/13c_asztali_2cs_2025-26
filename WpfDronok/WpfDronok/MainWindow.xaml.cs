fa144324255c7301491ad283d3392551e2fb7fc0
kovacsrud <kovacs.rud@gmail.com>
2025-09-08 11:09:45 +0200

    Drónok

---
+++ WpfDronok/WpfDronok/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDronok.Model;
+using WpfDronok.Views;
+
+namespace WpfDronok;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public List<Dron> Dronok { get; set; }=new List<Dron>();
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void menuitemKilepes_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+
+    private void menuitemNevjegy_Click(object sender, RoutedEventArgs e)
+    {
+        NevjegyView nevjegy=new NevjegyView();
+        nevjegy.ShowDialog();
+    }
+
+    private void menuitemMegnyitas_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        dialog.Filter = ".csv fájlok|*.csv|minden fájl|*.*";
+
+        if (dialog.ShowDialog()==true)
+        {
+            try
+            {
+                Dronok = new DronLista(dialog.FileName, ',').Dronok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+    }
+
+    private void menuitemTipusSzures_Click(object sender, RoutedEventArgs e)
+    {
+        TipusSzuresView tipusSzures = new TipusSzuresView(Dronok);
+        tipusSzures.ShowDialog();
+    }
+}
---
+++ WpfDronok/WpfDronok/Views/TipusSzuresView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDronok.Model;
+
+namespace WpfDronok.Views
+{
+    /// <summary>
+    /// Interaction logic for TipusSzuresView.xaml
+    /// </summary>
+    public partial class TipusSzuresView : Window
+    {
+        public List<Dron> Dronok { get; set; }=new List<Dron>();
+        public TipusSzuresView(List<Dron> dronok)
+        {
+            InitializeComponent();
+            Dronok= dronok;
+            datagridDronok.ItemsSource = Dronok;
+        }
+
+        private void buttonKeres_Click(object sender, RoutedEventArgs e)
+        {
+            var keresett = textboxKereses.Text;
+            var eredmeny=Dronok.FindAll(x=>x.Tipus==keresett);
+
+            if(eredmeny.Count()>0)
+            {
+                datagridDronok.ItemsSource= eredmeny;
+            } else
+            {
+                MessageBox.Show("Nincs ilyen adat!");
+            }
+        }
+    }
+}
