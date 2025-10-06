ec3382b34f8db45483b63aceb928f7f5a2bbe270
kovacsrud <kovacs.rud@gmail.com>
2025-10-06 08:38:38 +0200

    ViewModel készítése

---
+++ WpfKutyakDB/WpfKutyakDB/Mvvm/View/ViewKutyanevek.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfKutyakDB.Mvvm.View
+{
+    /// <summary>
+    /// Interaction logic for ViewKutyanevek.xaml
+    /// </summary>
+    public partial class ViewKutyanevek : Window
+    {
+        public ViewKutyanevek()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUjKutyanev_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonTorolKutyanev_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfKutyakDB/WpfKutyakDB/Mvvm/ViewModel/RendeloViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using WpfKutyakDB.Mvvm.Model;
+
+namespace WpfKutyakDB.Mvvm.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class RendeloViewModel
+    {
+        public List<Kutyanev> Kutyanevek { get; set; }= new List<Kutyanev>();
+        public Kutyanev SelectedKutyanev { get; set; } = new Kutyanev();
+
+        public RendeloViewModel()
+        {
+            GetKutyanevek();     
+        }
+
+        public void GetKutyanevek()
+        {
+            Kutyanevek = DbRepo.GetKutyanevek();
+        }
+
+        public void UjKutyanev(Kutyanev kutyanev)
+        {
+
+        }
+    }
+}
