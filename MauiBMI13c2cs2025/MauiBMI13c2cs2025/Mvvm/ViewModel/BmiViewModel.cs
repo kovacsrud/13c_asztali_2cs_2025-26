ee93237c50a89d0f71ef35025754cdf5adf2320f
kovacsrud <kovacs.rud@gmail.com>
2025-11-03 10:20:28 +0100

    BMI index számító MAUI program

---
+++ MauiBMI13c2cs2025/MauiBMI13c2cs2025/Mvvm/ViewModel/BmiViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace MauiBMI13c2cs2025.Mvvm.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BmiViewModel
+    {
+        public double BmiErtek { get; set; } = 0;
+        public string BmiSzoveg { get; set; } = "";
+        public double TestTomeg { get; set; } = 0;
+        public double Magassag { get; set; } = 0;
+
+        public void BmiSzamol()
+        {
+            BmiErtek = TestTomeg /Math.Pow(Magassag/100,2);
+            BmiSzoveges();
+        }
+
+        public void BmiSzoveges()
+        {
+
+        }
+    }
+}
