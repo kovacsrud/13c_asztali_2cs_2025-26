2218cd7da450db079106fa311f02f9bbf155e6be
kovacsrud <kovacs.rud@gmail.com>
2025-11-04 13:01:53 +0100

    Formázott collectionview

---
+++ MauiCollection13c2cs2025/MauiCollection13c2cs2025/ListaPage.xaml.cs
@@ -0,0 +1,30 @@
+using System.Collections.ObjectModel;
+
+namespace MauiCollection13c2cs2025;
+
+public partial class ListaPage : ContentPage
+{
+    ObservableCollection<Hegy> Hegyek { get; set; } = new ObservableCollection<Hegy>();
+    public ListaPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadMauiAsset();
+        BindingContext = Hegyek;
+    }
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("hegyekMo.txt");
+        using var reader = new StreamReader(stream);
+        reader.ReadLine();
+        while (!reader.EndOfStream)
+        {
+            Hegyek.Add(new Hegy(reader.ReadLine()));
+        }
+    }
+}
