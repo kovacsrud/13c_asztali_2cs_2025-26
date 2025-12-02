5156532b8611d3b8f9517c3f83680f37ee939f73
kovacsrud <kovacs.rud@gmail.com>
2025-12-02 08:54:50 +0100

    DbConfig osztály létrehozása

---
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/DbConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiJegyzet13c22025V2
+{
+    public static class DbConfig
+    {
+        private const string dbname = "jegyzetek.db";
+        public const SQLiteOpenFlags Flags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath {
+            get {
+                return Path.Combine(AppDomain.CurrentDomain.BaseDirectory,dbname);
+            }
+        }
+
+
+
+
+    }
+}
