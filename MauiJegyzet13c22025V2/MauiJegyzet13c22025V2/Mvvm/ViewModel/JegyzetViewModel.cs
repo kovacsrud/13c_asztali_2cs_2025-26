c34e255f1307f9e6d4c99180d49d4fc7d590a2eb
kovacsrud <kovacs.rud@gmail.com>
2025-12-02 10:28:41 +0100

    ViewModel létrehozása

---
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/ViewModel/JegyzetViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiJegyzet13c22025V2.Mvvm.Model;
+using PropertyChanged;
+
+namespace MauiJegyzet13c22025V2.Mvvm.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class JegyzetViewModel
+    {
+        public List<Jegyzet> Jegyzetek { get; set; }=new List<Jegyzet>();
+        public Jegyzet AktualisJegyzet { get; set; }
+
+
+        public JegyzetViewModel()
+        {
+            GetJegyzetek();
+        }
+
+        private void GetJegyzetek()
+        {
+            Jegyzetek = App.JegyzetRepo.GetItems();
+        }
+    }
+}
