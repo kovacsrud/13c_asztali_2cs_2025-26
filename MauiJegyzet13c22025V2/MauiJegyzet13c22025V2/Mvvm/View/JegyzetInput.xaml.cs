8eb1403bedcfc198b1879560681cf8a4a2657911
kovacsrud <kovacs.rud@gmail.com>
2025-12-02 11:40:36 +0100

    Módosítás kódolása

--- MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/View/JegyzetInput.xaml.cs
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/View/JegyzetInput.xaml.cs
@@ -11,18 +11,34 @@ public partial class JegyzetInput : ContentPage
 		InitializeComponent();
 	}
 
-    private void buttonInput_Clicked(object sender, EventArgs e)
+    public JegyzetInput(bool modosit,JegyzetViewModel vm)
+    {
+		InitializeComponent();
+		this.modosit = modosit;
+		BindingContext = vm;
+    }
+
+    private async void buttonInput_Clicked(object sender, EventArgs e)
     {
 		var vm = BindingContext as JegyzetViewModel;
 		if (modosit) {
 			labelCim.Text = "Jegyzet módosítása";
+			var result =await DisplayAlert("Módosítás", "Biztosan módosítja?", "Igen", "Nem");
+			if (result)
+			{
+				App.JegyzetRepo.UpdateItem(vm.AktualisJegyzet);
+                await DisplayAlert("Módosítás", App.JegyzetRepo.StatusMsg, "Ok");
+				vm.GetJegyzetek();
+
+            }
 
 		} else
 		{
 			var ujJegyzet=new Jegyzet { Cim=entryCim.Text, Szoveg=entrySzoveg.Text };
 			App.JegyzetRepo.NewItem(ujJegyzet);
-			DisplayAlert("Új jegyzet",App.JegyzetRepo.StatusMsg,"Ok");
+			await DisplayAlert("Új jegyzet",App.JegyzetRepo.StatusMsg,"Ok");
 			vm.GetJegyzetek();
+
 		}
 
 
--- MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/View/JegyzetView.xaml.cs
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/View/JegyzetView.xaml.cs
@@ -1,3 +1,5 @@
+using MauiJegyzet13c22025V2.Mvvm.ViewModel;
+
 namespace MauiJegyzet13c22025V2.Mvvm.View;
 
 public partial class JegyzetView : ContentPage
@@ -5,11 +7,13 @@ public partial class JegyzetView : ContentPage
 	public JegyzetView()
 	{
 		InitializeComponent();
+        BindingContext = new JegyzetViewModel();
 	}
 
     private void buttonUj_Clicked(object sender, EventArgs e)
     {
-
+        var vm=BindingContext as JegyzetViewModel;
+        Navigation.PushAsync(new JegyzetInput { BindingContext=vm });
     }
 
     private void buttonModosit_Clicked(object sender, EventArgs e)
