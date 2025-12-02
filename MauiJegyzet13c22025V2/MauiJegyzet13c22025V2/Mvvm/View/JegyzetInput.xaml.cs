71706aabf4e97330aac843e831bceded6dee1e28
kovacsrud <kovacs.rud@gmail.com>
2025-12-02 10:58:51 +0100

    Input oldal elkészítése

---
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Mvvm/View/JegyzetInput.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiJegyzet13c22025V2.Mvvm.View;
+
+public partial class JegyzetInput : ContentPage
+{
+	bool modosit = false;
+	public JegyzetInput()
+	{
+		InitializeComponent();
+	}
+
+    private void buttonInput_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
