4caea8f0c2e4561619569cef539018c7ae2acf92
kovacsrud <kovacs.rud@gmail.com>
2025-12-02 09:04:48 +0100

    TableData osztály létrehozása

---
+++ MauiJegyzet13c22025V2/MauiJegyzet13c22025V2/Intefaces/TableData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiJegyzet13c22025V2.Intefaces
+{
+    public class TableData
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+    }
+}
