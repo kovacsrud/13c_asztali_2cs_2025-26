eb7c370312059b012d39b41e900ed0cff78445d9
kovacsrud <kovacs.rud@gmail.com>
2026-02-10 10:56:22 +0100

    Adatbázis kezelés orm használatával

---
+++ WpfKutyakEF/WpfKutyakEF/mvvm/viewmodel/KutyaViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using PropertyChanged;
+using WpfKutyakEF.mvvm.model;
+
+namespace WpfKutyakEF.mvvm.viewmodel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class KutyaViewModel
+    {
+        KutyakGoodUniqueContext context;
+        public ObservableCollection<Kutya> Kutyak {  get; set; }
+        public ObservableCollection<Kutyafajtak> Kutyafajtak { get; set; }
+        public ObservableCollection<Kutyanevek> Kutyanevek { get; set; }
+
+        public Kutya SelectedKutya { get; set; }
+
+        public KutyaViewModel()
+        {
+            context = new KutyakGoodUniqueContext();
+            context.Kutyas.Load();
+            context.Kutyaneveks.Load();
+            context.Kutyafajtaks.Load();
+
+            Kutyak = context.Kutyas.Local.ToObservableCollection();
+            Kutyafajtak = context.Kutyafajtaks.Local.ToObservableCollection();
+            Kutyanevek = context.Kutyaneveks.Local.ToObservableCollection();
+        
+        }
+
+        public void DbMentes()
+        {
+            try
+            {
+                var result = context.SaveChanges();
+                if (result > 0)
+                {
+                    MessageBox.Show("Adatok mentve!");
+                } else
+                {
+                    MessageBox.Show("Nem változtak az adatok!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+    }
+}
