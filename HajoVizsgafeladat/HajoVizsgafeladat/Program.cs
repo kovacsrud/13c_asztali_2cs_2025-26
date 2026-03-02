d1dd8adc1585ec0152c96c4dc820d075c9d50a43
kovacsrud <kovacs.rud@gmail.com>
2026-03-02 14:19:58 +0100

    Kész

--- HajoVizsgafeladat/HajoVizsgafeladat/Program.cs
+++ HajoVizsgafeladat/HajoVizsgafeladat/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            
+            List<Hajo> hajok=new List<Hajo>();
+
+            hajok.Add(new Vitorlas
+            {
+                GyartasEve = 1980,
+                Hossz = 16,
+                MaxSebesseg = 20,
+                Nev = "V1",
+                Suly = 5,
+                VitorlakSzama = 6
+            });
+
+            hajok.Add(new Vitorlas
+            {
+                GyartasEve = 1960,
+                Hossz = 19,
+                MaxSebesseg = 22,
+                Nev = "V2",
+                Suly = 7,
+                VitorlakSzama = 10
+            });
+
+            hajok.Add(new Teher {
+                GyartasEve=1990,
+                Hossz=30,
+                MaxSebesseg=18,
+                Nev="T1",
+                Suly=15000,
+                SzallithatoTomeg=8000
+            });
+
+            hajok.Add(new Teher
+            {
+                GyartasEve = 1971,
+                Hossz = 35,
+                MaxSebesseg = 16,
+                Nev = "T2",
+                Suly = 25000,
+                SzallithatoTomeg = 12000
+            });
+
+            foreach (var i in hajok)
+            {
+                Console.WriteLine(i.ToString());
+            }
         }
     }
 }
---
+++ HajoVizsgafeladat/HajoVizsgafeladat/Vitorlas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajoVizsgafeladat
+{
+    public class Vitorlas : Hajo
+    {
+        public int VitorlakSzama { get; set; }
+        public override void Haladas()
+        {
+            Console.WriteLine("A vitorlás a szél segítségével siklik a vízen.");
+        }
+
+        public override void Leiras()
+        {
+            Console.WriteLine("Könnyű vitorlás hajó, tengeri kalandokhoz.");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+$" Vitorlák száma:{VitorlakSzama}";
+        }
+    }
+}
