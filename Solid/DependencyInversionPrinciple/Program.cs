c19c8018e4722fdbeb10303b7007b3fecd95eb29
kovacsrud <kovacs.rud@gmail.com>
2026-03-23 12:47:34 +0100

    Solid

---
+++ Solid/DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DependencyInversionPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dependency Inversion Principle");
+
+            //A magas szintű modulok ne függjenek az alacsony szintű moduloktól. Mindkettő absztrakcióktól függjön.
+
+            //Fixen "bedrótozott" adatbázis
+            var rosszUserService=new RosszUserService();
+
+            //Nincs fix adatbázis, bármi átadható, ami az IDatabase-t megvalósítja
+            var userService = new JoUserService(new OracleDB());
+            userService.UserMentes("Béla");
+
+
+        }
+    }
+}
